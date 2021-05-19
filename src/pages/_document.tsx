60b75df78fcbdaee98f4c641cf0315de691a6f34
Jamie Davies <jamie.davies@madetech.com>
2021-05-19 10:09:01 +0100

    Add suggested GOV.UK Design System meta tags to base document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 
-export default class GovUkDocument extends Document {
+const GovUkMetadata = () => (
+  <>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta name="theme-color" content="#0b0c0c" />
+
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+    <link rel="shortcut icon" sizes="16x16 32x32 48x48" href="/assets/images/favicon.ico" type="image/x-icon" />
+    <link rel="mask-icon" href="/assets/images/govuk-mask-icon.svg" color="#0b0c0c" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/govuk-apple-touch-icon-180x180.png" />
+    <link rel="apple-touch-icon" sizes="167x167" href="/assets/images/govuk-apple-touch-icon-167x167.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/govuk-apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" href="/assets/images/govuk-apple-touch-icon.png" />
+
+    <meta property="og:image" content="/assets/images/govuk-opengraph-image.png" />
+  </>
+)
+
+class GovUkDocument extends Document {
   render() {
     return (
       <Html className="govuk-template" lang="en">
-        <Head />
+        <Head>
+          <GovUkMetadata />
+        </Head>
+
         <body className="govuk-template__body">
           <Main />
           <NextScript />
@@ -13,3 +35,5 @@ export default class GovUkDocument extends Document {
     )
   }
 }
+
+export default GovUkDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 import Button from "components/Button"
 import GridRow from "components/GridRow"
 import Layout from "components/Layout"
+import Head from "next/head"
 import TextInput from "components/TextInput"
 
 const Index = () => (
-  <Layout>
-    <GridRow>
-      <h1 className="govuk-heading-xl">{"Sign in to Bichard 7"}</h1>
-      <form>
-        <TextInput id="email" name="email" label="Email address" type="email" />
-        <TextInput id="password" name="password" label="Password" type="password" />
-        <Button>{"Sign in"}</Button>
-      </form>
-    </GridRow>
-  </Layout>
+  <>
+    <Head>
+      <title>{"Sign in to Bichard 7"}</title>
+    </Head>
+    <Layout>
+      <GridRow>
+        <h1 className="govuk-heading-xl">{"Sign in to Bichard 7"}</h1>
+        <form>
+          <TextInput id="email" name="email" label="Email address" type="email" />
+          <TextInput id="password" name="password" label="Password" type="password" />
+          <Button>{"Sign in"}</Button>
+        </form>
+      </GridRow>
+    </Layout>
+  </>
 )
 
 export default Index
