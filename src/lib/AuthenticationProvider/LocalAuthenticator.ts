327171193fc8652b33de831eacfbe88e1baa633a
Jamie Davies <jamie.davies@madetech.com>
2021-05-25 16:45:15 +0100

    Allow configuring JWT secret and expiry with environment variables

--- src/lib/AuthenticationProvider/LocalAuthenticator.ts
+++ src/lib/AuthenticationProvider/LocalAuthenticator.ts
@@ -2,17 +2,19 @@ import AuthenticationProvider from "lib/AuthenticationProvider"
 import { User, UserCredentials } from "lib/User"
 import { AuthenticationResult, Token } from "lib/AuthenticationResult"
 import jwt from "jsonwebtoken"
+import config from "lib/config"
 
 export default class LocalAuthenticator implements AuthenticationProvider {
   private static readonly users: [UserCredentials] = [{ emailAddress: "bichard01@example.com", password: "password" }]
 
+  // eslint-disable-next-line class-methods-use-this
   public authenticate(credentials: UserCredentials): AuthenticationResult {
     const [match] = LocalAuthenticator.users.filter(
       (u) => u.emailAddress === credentials.emailAddress && u.password === credentials.password
     )
 
     if (match) {
-      return this.generateToken({
+      return LocalAuthenticator.generateToken({
         emailAddress: match.emailAddress
       })
     }
@@ -20,8 +22,10 @@ export default class LocalAuthenticator implements AuthenticationProvider {
     return new Error("Invalid credentials")
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  private generateToken(user: User): Token {
-    return jwt.sign(user, "super-secret-secret", { expiresIn: "10 seconds" })
+  private static generateToken(user: User): Token {
+    const options = {
+      expiresIn: config.localAuthenticator.jwtExpiresIn
+    }
+    return jwt.sign(user, config.localAuthenticator.jwtSecret, options)
   }
 }
