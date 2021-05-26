622da0f0195f1c17c29fc7640fa49cd6eb9d87b3
Jamie Davies <jamie.davies@madetech.com>
2021-05-26 15:44:28 +0100

    Add in more complete test user data
    This (mostly) has been copied over from the postgres database
    seeds/migrations at from `USERS.csv`, `USERS_ALL.csv`,
    `USERS_GROUPS.csv`, `USERS_GROUPS_ALL.csv` and `GROUPS.csv` from:
    
    https://github.com/ministryofjustice/bichard7-next/tree/8d095974a1cc9aa61e9b45a61eb01060ec51991f/scripts/postgres/liquibase/seeds

--- src/lib/AuthenticationProvider/LocalAuthenticator.ts
+++ src/lib/AuthenticationProvider/LocalAuthenticator.ts
@@ -1,43 +1,35 @@
 import AuthenticationProvider from "lib/AuthenticationProvider"
 import { User, UserCredentials } from "lib/User"
-import { AuthenticationResult, Token } from "lib/AuthenticationResult"
+import { AuthenticationResult, Token, TokenPayload } from "lib/AuthenticationResult"
 import jwt from "jsonwebtoken"
 import config from "lib/config"
+import users from "data/users"
 
 export default class LocalAuthenticator implements AuthenticationProvider {
-  private static readonly users: Array<UserCredentials & User> = [
-    {
-      emailAddress: "bichard01@example.com",
-      password: "password",
-      displayName: "Bichard01",
-      role: "B7Allocator"
-    },
-    {
-      emailAddress: "b7exceptionhandler@example.com",
-      password: "password",
-      displayName: "B7ExceptionHandler",
-      role: "B7ExceptionHandler"
-    }
-  ]
-
   // eslint-disable-next-line class-methods-use-this
   public authenticate(credentials: UserCredentials): AuthenticationResult {
-    const [match] = LocalAuthenticator.users.filter(
+    const [match] = users.filter(
       (u) => u.emailAddress === credentials.emailAddress && u.password === credentials.password
     )
 
-    if (match) {
-      const { displayName, emailAddress, role } = match
-      return LocalAuthenticator.generateToken({ displayName, emailAddress, role })
-    }
-
-    return new Error("Invalid credentials")
+    return match ? LocalAuthenticator.generateToken(match) : new Error("Invalid credentials")
   }
 
   private static generateToken(user: User): Token {
-    const options = {
+    const payload: TokenPayload = {
+      username: user.username,
+      exclusionList: user.exclusionList,
+      inclusionList: user.inclusionList,
+      forenames: user.forenames,
+      surname: user.surname,
+      emailAddress: user.emailAddress,
+      groups: user.groups
+    }
+
+    const options: jwt.SignOptions = {
       expiresIn: config.localAuthenticator.jwtExpiresIn
     }
-    return jwt.sign(user, config.localAuthenticator.jwtSecret, options)
+
+    return jwt.sign(payload, config.localAuthenticator.jwtSecret, options)
   }
 }
--- test/lib/AuthenticationProvider/LocalAuthenticator.test.ts
+++ test/lib/AuthenticationProvider/LocalAuthenticator.test.ts
@@ -1,6 +1,7 @@
 import LocalAuthenticator from "lib/AuthenticationProvider/LocalAuthenticator"
 import jwt from "jsonwebtoken"
 import config from "lib/config"
+import { TokenPayload } from "lib/AuthenticationResult"
 
 describe("Local development authenticator", () => {
   const authenticator = new LocalAuthenticator()
@@ -19,9 +20,21 @@ describe("Local development authenticator", () => {
   })
 
   test.each`
-    emailAddress                        | password
-    ${"bichard01@example.com"}          | ${"password"}
-    ${"b7exceptionhandler@example.com"} | ${"password"}
+    emailAddress                       | password
+    ${"bichard01@example.com"}         | ${"password"}
+    ${"allocator1@example.com"}        | ${"password"}
+    ${"audit1@example.com"}            | ${"password"}
+    ${"exceptionhandler1@example.com"} | ${"password"}
+    ${"generalhandler1@example.com"}   | ${"password"}
+    ${"supervisor1@example.com"}       | ${"password"}
+    ${"triggerhandler1@example.com"}   | ${"password"}
+    ${"allocator2@example.com"}        | ${"password"}
+    ${"audit2@example.com"}            | ${"password"}
+    ${"exceptionhandler2@example.com"} | ${"password"}
+    ${"generalhandler2@example.com"}   | ${"password"}
+    ${"supervisor2@example.com"}       | ${"password"}
+    ${"triggerhandler2@example.com"}   | ${"password"}
+    ${"nogroupsassigned@example.com"}  | ${"password"}
   `("should authenticate $emailAddress:$password", ({ emailAddress, password }) => {
     const result = authenticator.authenticate({ emailAddress, password })
     expect(result).not.toBeInstanceOf(Error)
@@ -30,12 +43,24 @@ describe("Local development authenticator", () => {
 
   it("should generate a valid JWT token for an authenticated user", () => {
     const token = authenticator.authenticate({ emailAddress: "bichard01@example.com", password: "password" })
-    const payload = jwt.verify(token as string, config.localAuthenticator.jwtSecret)
+    const data = jwt.verify(token as string, config.localAuthenticator.jwtSecret)
 
-    expect(payload).toHaveProperty("emailAddress")
-    expect(payload).toHaveProperty("role")
-    expect(payload).toHaveProperty("displayName")
-    expect(payload).toHaveProperty("exp")
-    expect(payload).not.toHaveProperty("password")
+    expect(data).not.toHaveProperty("password")
+
+    const payload = data as TokenPayload
+    expect(payload.username).toEqual("Bichard01")
+    expect(payload.exclusionList).toEqual(["5", "6", "7", "8"])
+    expect(payload.inclusionList).toEqual(["B41ME00"])
+    expect(payload.forenames).toEqual("Bichard User")
+    expect(payload.surname).toEqual("01")
+    expect(payload.emailAddress).toEqual("bichard01@example.com")
+    expect(payload.groups).toEqual([
+      "B7Allocator",
+      "B7Audit",
+      "B7ExceptionHandler",
+      "B7GeneralHandler",
+      "B7Supervisor",
+      "B7TriggerHandler"
+    ])
   })
 })
