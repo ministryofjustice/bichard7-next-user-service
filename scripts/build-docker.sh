bf0861c4c35a11e0c2dc71677a466f49e0fcf456
Jamie Davies <jamie.davies@madetech.com>
2021-05-28 11:28:41 +0100

    Wrap shell variable access in a string

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -14,7 +14,7 @@ fi
 aws ecr get-login-password --region eu-west-2 | docker login \
     --username AWS \
     --password-stdin \
-    ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
+    "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com"
 
 # Get our latest staged nodejs image
 IMAGE_HASH=$(aws ecr describe-images \
