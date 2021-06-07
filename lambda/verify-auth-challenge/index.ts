b007137ebed2cdf4e6d4fed59746ebbacc11a1c6
Jamie Davies <jamie.davies@madetech.com>
2021-06-07 16:08:19 +0100

    Add lambda trigger to verify secret code

---
+++ lambda/verify-auth-challenge/index.ts
@@ -0,0 +1,13 @@
+import { VerifyAuthChallengeResponseTriggerHandler } from "aws-lambda"
+
+export const handler: VerifyAuthChallengeResponseTriggerHandler = async (event) => {
+  const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode
+
+  if (event.request.challengeAnswer === expectedAnswer) {
+    event.response.answerCorrect = true
+  } else {
+    event.response.answerCorrect = false
+  }
+
+  return event
+}
