b731af04c76ccd3e19bbe48308c00496bf739834
Jamie Davies <jamie.davies@madetech.com>
2021-06-08 15:49:30 +0100

    Get username/password auth via AWS SDK working

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,8 +4,19 @@ interface LocalAuthenticatorConfig {
   jwtExpiresIn: string
 }
 
+interface CognitoAuthenticatorConfig {
+  clientId: string
+  clientSecret: string
+  region: string
+  userPoolId: string
+  accessKeyId: string
+  secretAccessKey: string
+  sessionToken: string
+}
+
 interface UserServiceConfig {
   bichardRedirectURL: string
+  cognitoAuthenticator: CognitoAuthenticatorConfig
   localAuthenticator: LocalAuthenticatorConfig
   tokenQueryParamName: string
 }
@@ -17,6 +28,15 @@ const config: UserServiceConfig = {
     jwtIssuer: process.env.LOCAL_AUTH_TOKEN_ISSUER ?? "Bichard",
     jwtSecret: process.env.LOCAL_AUTH_TOKEN_SECRET ?? "OliverTwist",
     jwtExpiresIn: process.env.LOCAL_AUTH_TOKEN_EXPIRES_IN ?? "5 seconds"
+  },
+  cognitoAuthenticator: {
+    clientId: process.env.COGNITO_CLIENT_ID ?? "",
+    clientSecret: process.env.COGNITO_CLIENT_SECRET ?? "",
+    region: process.env.COGNITO_REGION || "eu-west-2",
+    userPoolId: process.env.COGNITO_USER_POOL_ID ?? "",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+    sessionToken: process.env.AWS_SESSION_TOKEN ?? ""
   }
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,48 +4,74 @@ import GridRow from "components/GridRow"
 import Layout from "components/Layout"
 import Head from "next/head"
 import TextInput from "components/TextInput"
-import Authenticator from "lib/Authenticator"
 import { GetServerSideProps } from "next"
 import parseFormData from "lib/parseFormData"
 import { UserCredentials } from "lib/User"
-import { isSuccess } from "lib/AuthenticationResult"
 import config from "lib/config"
+import { AdminInitiateAuthCommand, CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider"
+import crypto from "crypto"
+
+const generateSecretHash = (username: string) =>
+  crypto
+    .createHmac("SHA256", config.cognitoAuthenticator.clientSecret)
+    .update(username + config.cognitoAuthenticator.clientId)
+    .digest("base64")
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   let invalidCredentials = false
+  let token = ""
 
   if (req.method === "POST") {
     const credentials: UserCredentials = (await parseFormData(req)) as { emailAddress: string; password: string }
 
     if (credentials.emailAddress && credentials.password) {
-      const result = Authenticator.authenticate(credentials)
+      const client = new CognitoIdentityProviderClient({
+        credentials: {
+          accessKeyId: config.cognitoAuthenticator.accessKeyId,
+          secretAccessKey: config.cognitoAuthenticator.secretAccessKey,
+          sessionToken: config.cognitoAuthenticator.sessionToken
+        },
+        region: config.cognitoAuthenticator.region
+      })
 
-      if (isSuccess(result)) {
-        const url = new URL(config.bichardRedirectURL)
-        url.searchParams.append(config.tokenQueryParamName, result)
+      const command = new AdminInitiateAuthCommand({
+        AuthFlow: "ADMIN_NO_SRP_AUTH",
+        AuthParameters: {
+          USERNAME: credentials.emailAddress,
+          PASSWORD: credentials.password,
+          SECRET_HASH: generateSecretHash(credentials.emailAddress)
+        },
+        ClientId: config.cognitoAuthenticator.clientId,
+        UserPoolId: config.cognitoAuthenticator.userPoolId
+      })
 
-        return {
-          redirect: {
-            destination: url.href,
-            statusCode: 302
-          }
-        }
+      try {
+        const response = await client.send(command)
+        console.log(response)
+        token = response.AuthenticationResult?.AccessToken || ""
+      } catch (error) {
+        console.error("Cognito failed", error)
+        invalidCredentials = true
       }
+    } else {
+      invalidCredentials = true
     }
-
-    invalidCredentials = true
   }
 
   return {
-    props: { invalidCredentials }
+    props: {
+      invalidCredentials,
+      token
+    }
   }
 }
 
 interface Props {
   invalidCredentials?: boolean
+  token?: string
 }
 
-const Index = ({ invalidCredentials }: Props) => (
+const Index = ({ invalidCredentials, token }: Props) => (
   <>
     <Head>
       <title>{"Sign in to Bichard 7"}</title>
@@ -66,6 +92,12 @@ const Index = ({ invalidCredentials }: Props) => (
           <Button>{"Sign in"}</Button>
         </form>
       </GridRow>
+
+      {token && (
+        <GridRow>
+          <code style={{ wordWrap: "break-word" }}>{token}</code>
+        </GridRow>
+      )}
     </Layout>
   </>
 )
