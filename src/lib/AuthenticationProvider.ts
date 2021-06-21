5bebbb39f61c67b98f4450add85d9a9cdf9a9d2f
Jamie Davies <jamie.davies@madetech.com>
2021-06-21 15:32:45 +0100

    Turn AuthenticationProvider interface into an abstract class
    This will allow common functionality - such as the generation of tokens
    - to be shared between all AuthenticationProviders.

--- src/lib/AuthenticationProvider.ts
+++ src/lib/AuthenticationProvider.ts
@@ -1,6 +1,28 @@
-import { UserCredentials } from "./User"
-import { AuthenticationResult } from "./AuthenticationResult"
+import jwt from "jsonwebtoken"
+import config from "lib/config"
+import { AuthenticationResult } from "lib/AuthenticationResult"
+import { Token, TokenPayload } from "lib/Token"
+import { User, UserCredentials } from "lib/User"
 
-export default interface AuthenticationProvider {
-  authenticate(credentials: UserCredentials): AuthenticationResult
+export default abstract class AuthenticationProvider {
+  abstract authenticate(credentials: UserCredentials): AuthenticationResult
+
+  protected static generateToken(user: User): Token {
+    const payload: TokenPayload = {
+      username: user.username,
+      exclusionList: user.exclusionList,
+      inclusionList: user.inclusionList,
+      forenames: user.forenames,
+      surname: user.surname,
+      emailAddress: user.emailAddress,
+      groups: user.groups
+    }
+
+    const options: jwt.SignOptions = {
+      expiresIn: config.localAuthenticator.jwtExpiresIn,
+      issuer: config.localAuthenticator.jwtIssuer
+    }
+
+    return jwt.sign(payload, config.localAuthenticator.jwtSecret, options)
+  }
 }
--- src/lib/AuthenticationProvider/LocalAuthenticator.ts
+++ src/lib/AuthenticationProvider/LocalAuthenticator.ts
@@ -1,37 +1,15 @@
 import AuthenticationProvider from "lib/AuthenticationProvider"
-import { User, UserCredentials } from "lib/User"
 import { AuthenticationResult } from "lib/AuthenticationResult"
-import { Token, TokenPayload } from "lib/Token"
-import jwt from "jsonwebtoken"
-import config from "lib/config"
+import { UserCredentials } from "lib/User"
 import users from "data/users"
 
-export default class LocalAuthenticator implements AuthenticationProvider {
+export default class LocalAuthenticator extends AuthenticationProvider {
   // eslint-disable-next-line class-methods-use-this
   public authenticate(credentials: UserCredentials): AuthenticationResult {
     const [match] = users.filter(
       (u) => u.emailAddress === credentials.emailAddress && u.password === credentials.password
     )
 
-    return match ? LocalAuthenticator.generateToken(match) : new Error("Invalid credentials")
-  }
-
-  private static generateToken(user: User): Token {
-    const payload: TokenPayload = {
-      username: user.username,
-      exclusionList: user.exclusionList,
-      inclusionList: user.inclusionList,
-      forenames: user.forenames,
-      surname: user.surname,
-      emailAddress: user.emailAddress,
-      groups: user.groups
-    }
-
-    const options: jwt.SignOptions = {
-      expiresIn: config.localAuthenticator.jwtExpiresIn,
-      issuer: config.localAuthenticator.jwtIssuer
-    }
-
-    return jwt.sign(payload, config.localAuthenticator.jwtSecret, options)
+    return match ? AuthenticationProvider.generateToken(match) : new Error("Invalid credentials")
   }
 }
