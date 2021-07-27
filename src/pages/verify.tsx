aa2428a9bc49bae8ef32a2045ce853ebaa164c92
Jamie Davies <jamie.davies@madetech.com>
2021-07-27 10:22:51 +0100

    Make link from email redirect to Bichard after correct password entry

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -5,20 +5,47 @@ import Layout from "components/Layout"
 import Head from "next/head"
 import TextInput from "components/TextInput"
 import { GetServerSideProps } from "next"
+import { UserCredentials } from "lib/User"
+import parseFormData from "lib/parseFormData"
+import { isSuccess } from "lib/AuthenticationResult"
+import config from "lib/config"
+import Authenticator from "lib/Authenticator"
 
-// eslint-disable-next-line require-await
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+  let invalidCredentials = false
   let invalidVerification = false
 
   const { emailAddress, verificationCode } = query
 
-  if (!emailAddress || !verificationCode) {
+  if (req.method === "POST") {
+    const credentials: UserCredentials = (await parseFormData(req)) as { emailAddress: string; password: string }
+
+    if (credentials.emailAddress && credentials.password) {
+      const result = await Authenticator.authenticate(credentials)
+
+      if (isSuccess(result)) {
+        const token = result
+        const url = new URL(config.bichardRedirectURL)
+        url.searchParams.append(config.tokenQueryParamName, token)
+
+        return {
+          redirect: {
+            destination: url.href,
+            statusCode: 302
+          }
+        }
+      }
+    }
+
+    invalidCredentials = true
+  } else if (!emailAddress || !verificationCode) {
     invalidVerification = true
   }
 
   return {
     props: {
       emailAddress,
+      invalidCredentials,
       invalidVerification
     }
   }
@@ -26,10 +53,11 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 
 interface Props {
   emailAddress?: string
+  invalidCredentials?: boolean
   invalidVerification?: boolean
 }
 
-const VerifyEmail = ({ emailAddress, invalidVerification }: Props) => (
+const VerifyEmail = ({ emailAddress, invalidCredentials, invalidVerification }: Props) => (
   <>
     <Head>
       <title>{"Sign in to Bichard 7"}</title>
@@ -44,8 +72,14 @@ const VerifyEmail = ({ emailAddress, invalidVerification }: Props) => (
           </ErrorSummary>
         )}
 
+        {invalidCredentials && (
+          <ErrorSummary title="Invalid credentials">
+            {"The supplied email address and password are not valid."}
+          </ErrorSummary>
+        )}
+
         {!invalidVerification && (
-          <form action="/" method="post">
+          <form action="/verify" method="post">
             <p className="govuk-body">
               {"You are signing in as "}
               <b>{emailAddress}</b>
