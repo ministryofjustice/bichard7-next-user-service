49fb3b37fc64d6d627893ba48764dde45043405a
Emad Karamad <emad.karamad@madetech.com>
2021-08-09 16:00:26 +0100

    Update Button component to support variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
 import { ReactNode } from "react"
 
+type ButtonVariant = "default" | "warning"
+
 interface Props {
   children: ReactNode
+  variant?: ButtonVariant
 }
 
-const Button = ({ children }: Props) => (
-  <button type="submit" className="govuk-button" data-module="govuk-button">
+const getButtonClass = (variant?: ButtonVariant): string => {
+  const variantClass = !variant || variant === "default" ? "" : ` govuk-button--${variant}`
+  return `govuk-button${variantClass}`
+}
+
+const Button = ({ children, variant }: Props) => (
+  <button type="submit" className={getButtonClass(variant)} data-module="govuk-button">
     {children}
   </button>
 )
