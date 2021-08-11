22e314e16a8199648f20272436bd1a8eb68c2125
Emad Karamad <emad.karamad@madetech.com>
2021-08-11 11:38:52 +0100

    Add username link back to users table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,12 @@
 import Layout from "components/Layout"
 import Head from "next/head"
-import { Table, TableHeaders } from "components/Table"
+import { LinkColumn, Table, TableHeaders } from "components/Table"
 import { GetServerSideProps } from "next"
 import { getAllUsers } from "useCases"
 import getConnection from "lib/getConnection"
 import isError from "lib/isError"
 import KeyValuePair from "types/KeyValuePair"
+import { User } from "lib/User"
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const connection = getConnection()
@@ -44,7 +45,13 @@ const Users = ({ allUsers }: Props) => (
     <Head>
       <title>{"Users"}</title>
     </Head>
-    <Layout>{allUsers && <Table tableHeaders={tableHeaders} tableTitle="Users" tableData={allUsers} />}</Layout>
+    <Layout>
+      {allUsers && (
+        <Table tableHeaders={tableHeaders} tableTitle="Users" tableData={allUsers}>
+          <LinkColumn field="username" href={(user) => `users/${(user as User).username}`} />
+        </Table>
+      )}
+    </Layout>
   </>
 )
 
