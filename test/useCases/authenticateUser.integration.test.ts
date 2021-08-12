9b2e95e866b49f356b02205758c287b6070e0801
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-08-12 10:45:47 +0100

    Spliting up logic for verification code

--- test/useCases/authenticateUser.integration.test.ts
+++ test/useCases/authenticateUser.integration.test.ts
@@ -1,7 +1,7 @@
-import { User, UserCredentials } from "lib/User"
-import db from "lib/db"
-import { storeVerificationCode } from "lib/sendVerificationEmail"
-import Authenticator from "lib/Authenticator"
+import getConnection from "lib/getConnection"
+import authenticate from "useCases/authenticate"
+import User from "types/User"
+import storeVerificationCode from "useCases/storeVerificationCode"
 
 const expectedUser = {
   username: "DummyUsername",
@@ -18,18 +18,19 @@ const expectedUser = {
 
 const correctPassword = "correctPassword"
 const invalidPassword = "invalidPassword"
+const connection = getConnection()
 
 describe("Authenticator", () => {
   beforeAll(async () => {
     const deleteQuery = `DELETE FROM br7own.users WHERE username = $1`
-    await db.none(deleteQuery, [expectedUser.username])
+    await connection.none(deleteQuery, [expectedUser.username])
 
     const insertQuery = `
         INSERT INTO br7own.users(
           username, email, active, exclusion_list, inclusion_list, challenge_response, created_at, endorsed_by, org_serves, forenames, surname, postal_address, post_code, phone_number, password)
           VALUES ($1, $2, true, $3, $4, '-', NOW(), $5, $6, $7, $8, $9, $10, $11, $12);
         `
-    await db.none(insertQuery, [
+    await connection.none(insertQuery, [
       expectedUser.username,
       expectedUser.emailAddress,
       expectedUser.exclusionList,
@@ -46,54 +47,34 @@ describe("Authenticator", () => {
   })
 
   afterAll(() => {
-    db.$pool.end()
+    connection.$pool.end()
   })
 
   it("should allow the user to authenticate with correct code and password", async () => {
     const verificationCode = "CoDe"
-    storeVerificationCode(expectedUser.emailAddress, verificationCode)
-    const userCredentials: UserCredentials = {
-      emailAddress: expectedUser.emailAddress,
-      password: correctPassword,
-      verificationCode
-    }
+    storeVerificationCode(connection, expectedUser.emailAddress, verificationCode)
 
-    const result = await Authenticator.authenticate(userCredentials)
+    const result = await authenticate(connection, expectedUser.emailAddress, correctPassword, verificationCode)
   })
 
   it("should not allow the user to authenticate with correct code and incorrect password", async () => {
     const verificationCode = "CoDe"
-    storeVerificationCode(expectedUser.emailAddress, verificationCode)
-    const userCredentials: UserCredentials = {
-      emailAddress: expectedUser.emailAddress,
-      password: invalidPassword,
-      verificationCode
-    }
+    storeVerificationCode(connection, expectedUser.emailAddress, verificationCode)
 
-    const result = await Authenticator.authenticate(userCredentials)
+    const result = await authenticate(connection, expectedUser.emailAddress, invalidPassword, verificationCode)
   })
 
   it("should not allow the user to authenticate with incorrect code and correct password", async () => {
     const verificationCode = "CoDe"
-    storeVerificationCode(expectedUser.emailAddress, verificationCode)
-    const userCredentials: UserCredentials = {
-      emailAddress: expectedUser.emailAddress,
-      password: correctPassword,
-      verificationCode: "SomethingElse"
-    }
+    storeVerificationCode(connection, expectedUser.emailAddress, verificationCode)
 
-    const result = await Authenticator.authenticate(userCredentials)
+    const result = await authenticate(connection, expectedUser.emailAddress, correctPassword, "SomethingElse")
   })
 
   it("should allow the user to authenticate with correct code and password only once", async () => {
     const verificationCode = "CoDe"
-    storeVerificationCode(expectedUser.emailAddress, verificationCode)
-    const userCredentials: UserCredentials = {
-      emailAddress: expectedUser.emailAddress,
-      password: correctPassword,
-      verificationCode
-    }
+    storeVerificationCode(connection, expectedUser.emailAddress, verificationCode)
 
-    const result = await Authenticator.authenticate(userCredentials)
+    const result = await authenticate(connection, expectedUser.emailAddress, correctPassword, verificationCode)
   })
 })
