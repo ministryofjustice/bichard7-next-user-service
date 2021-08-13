2f521a5acc79158322823c24957b59d24904589c
Emad Karamad <emad.karamad@madetech.com>
2021-08-13 16:13:11 +0100

    Refactor send verification email use cases

--- src/useCases/sendEmail.ts
+++ src/useCases/sendEmail.ts
@@ -1,8 +1,10 @@
-import { Result } from "types/Result"
+import { PromiseResult } from "types/Result"
 
-export default (emailAddress: string, subject: string, body: string): Result<void> => {
+export default (emailAddress: string, subject: string, body: string): PromiseResult<void> => {
   console.log("Recipient: ", emailAddress)
   console.log("Subject: ", subject)
   console.log("Body:")
   console.log(body)
+
+  return Promise.resolve()
 }
--- src/useCases/storeVerificationCode.ts
+++ src/useCases/storeVerificationCode.ts
@@ -1,18 +1,14 @@
 import Database from "types/Database"
+import PromiseResult from "types/PromiseResult"
 
-const storeVerificationCode = async (connection: Database, emailAddress: string, verificationCode: string) => {
+export default async (connection: Database, emailAddress: string, verificationCode: string): PromiseResult<void> => {
   const storeVerificationQuery = `
     UPDATE br7own.users
     SET email_verification_code = $1,
       email_verification_generated = NOW()
     WHERE email = $2 AND deleted_at IS NULL
   `
-  try {
-    await connection.none(storeVerificationQuery, [verificationCode, emailAddress])
-    return true
-  } catch (error) {
-    return error
-  }
-}
+  const result = await connection.none(storeVerificationQuery, [verificationCode, emailAddress]).catch((error) => error)
 
-export default storeVerificationCode
+  return result
+}
