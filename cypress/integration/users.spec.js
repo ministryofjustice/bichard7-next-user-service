f03d86dfd727028299c6bee4fd2f69f82d17a4ab
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-08-16 15:06:22 +0100

    Updating based on PR comments

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -25,8 +25,14 @@ describe("User", () => {
       cy.get("tbody tr:nth-child(3) td:nth-child(4)").should("have.text", "0800 333 444")
       cy.get("tbody tr:nth-child(3) td:nth-child(5)").should("have.text", "bichard03@example.com")
     })
+  })
 
-    it("should be able to create a new user", () => {
+  describe("Creation of new user", () => {
+    before(async () => {
+      await cy.task("seedUsers")
+    })
+
+    it("should be sccessful if inputs are correct", () => {
       cy.visit("users/newUser")
 
       cy.get('input[id="username"]').type("Buser")
@@ -41,10 +47,20 @@ describe("User", () => {
       cy.get('input[id="orgServes"]').type("B organisation")
 
       cy.get("button").click()
-      cy.get("h3").should("have.text", "User Buser has ben successfully created")
+      cy.get("h3").should("have.text", "User Buser has been successfully created")
     })
 
-    it("should be able to view a newly created user", () => {
+    it("should add new user in the list of users", () => {
+      cy.visit("users")
+
+      cy.get("tbody tr:nth-child(4) td:nth-child(1)").should("have.text", "Buser")
+      cy.get("tbody tr:nth-child(4) td:nth-child(2)").should("have.text", "B forename")
+      cy.get("tbody tr:nth-child(4) td:nth-child(3)").should("have.text", "B surname")
+      cy.get("tbody tr:nth-child(4) td:nth-child(4)").should("have.text", "B phone")
+      cy.get("tbody tr:nth-child(4) td:nth-child(5)").should("have.text", "bemail1@example.com")
+    })
+
+    it("doesn't update other users when a new user is created", () => {
       cy.visit("users")
 
       cy.get("tbody tr:nth-child(1) td:nth-child(1)").should("have.text", "Bichard01")
@@ -64,15 +80,9 @@ describe("User", () => {
       cy.get("tbody tr:nth-child(3) td:nth-child(3)").should("have.text", "Surname 03")
       cy.get("tbody tr:nth-child(3) td:nth-child(4)").should("have.text", "0800 333 444")
       cy.get("tbody tr:nth-child(3) td:nth-child(5)").should("have.text", "bichard03@example.com")
-
-      cy.get("tbody tr:nth-child(4) td:nth-child(1)").should("have.text", "Buser")
-      cy.get("tbody tr:nth-child(4) td:nth-child(2)").should("have.text", "B forename")
-      cy.get("tbody tr:nth-child(4) td:nth-child(3)").should("have.text", "B surname")
-      cy.get("tbody tr:nth-child(4) td:nth-child(4)").should("have.text", "B phone")
-      cy.get("tbody tr:nth-child(4) td:nth-child(5)").should("have.text", "bemail1@example.com")
     })
 
-    it("should not allow account with same username to be created", () => {
+    it("should fail if previously used username is given", () => {
       cy.visit("users/newUser")
 
       cy.get('input[id="username"]').type("Bichard01")
