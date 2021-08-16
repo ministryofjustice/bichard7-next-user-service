1fd883ffad4648e18692bfb8ea876052d35e5966
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-08-16 13:20:56 +0100

    Adding cypress tests for adding new user
    Also added filter logic to keep consistency on the sorted items in the list

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -25,5 +25,67 @@ describe("User", () => {
       cy.get("tbody tr:nth-child(3) td:nth-child(4)").should("have.text", "0800 333 444")
       cy.get("tbody tr:nth-child(3) td:nth-child(5)").should("have.text", "bichard03@example.com")
     })
+
+    it("should display new user after it was created", () => {
+      cy.visit("users/newUser")
+
+      cy.get('input[id="username"]').type("Buser")
+      cy.get('input[id="forenames"]').type("B forename")
+      cy.get('input[id="surname"]').type("B surname")
+      cy.get('input[id="phoneNumber"]').type("B phone")
+      cy.get('input[id="emailAddress"]').type("bemail1@example.com")
+
+      cy.get('input[id="postalAddress"]').type("B Address")
+      cy.get('input[id="postCode"]').type("B Code")
+      cy.get('input[id="endorsedBy"]').type("B Endorsed")
+      cy.get('input[id="orgServes"]').type("B organisation")
+
+      cy.get("button").click()
+      cy.get("h3").should("have.text", "User Buser has ben successfully created")
+
+      cy.visit("users")
+
+      cy.get("tbody tr:nth-child(1) td:nth-child(1)").should("have.text", "Bichard01")
+      cy.get("tbody tr:nth-child(1) td:nth-child(2)").should("have.text", "Bichard User 01")
+      cy.get("tbody tr:nth-child(1) td:nth-child(3)").should("have.text", "Surname 01")
+      cy.get("tbody tr:nth-child(1) td:nth-child(4)").should("have.text", "0800 111 222")
+      cy.get("tbody tr:nth-child(1) td:nth-child(5)").should("have.text", "bichard01@example.com")
+
+      cy.get("tbody tr:nth-child(2) td:nth-child(1)").should("have.text", "Bichard02")
+      cy.get("tbody tr:nth-child(2) td:nth-child(2)").should("have.text", "Bichard User 02")
+      cy.get("tbody tr:nth-child(2) td:nth-child(3)").should("have.text", "Surname 02")
+      cy.get("tbody tr:nth-child(2) td:nth-child(4)").should("have.text", "0800 222 333")
+      cy.get("tbody tr:nth-child(2) td:nth-child(5)").should("have.text", "bichard02@example.com")
+
+      cy.get("tbody tr:nth-child(3) td:nth-child(1)").should("have.text", "Bichard03")
+      cy.get("tbody tr:nth-child(3) td:nth-child(2)").should("have.text", "Bichard User 03")
+      cy.get("tbody tr:nth-child(3) td:nth-child(3)").should("have.text", "Surname 03")
+      cy.get("tbody tr:nth-child(3) td:nth-child(4)").should("have.text", "0800 333 444")
+      cy.get("tbody tr:nth-child(3) td:nth-child(5)").should("have.text", "bichard03@example.com")
+
+      cy.get("tbody tr:nth-child(4) td:nth-child(1)").should("have.text", "Buser")
+      cy.get("tbody tr:nth-child(4) td:nth-child(2)").should("have.text", "B forename")
+      cy.get("tbody tr:nth-child(4) td:nth-child(3)").should("have.text", "B surname")
+      cy.get("tbody tr:nth-child(4) td:nth-child(4)").should("have.text", "B phone")
+      cy.get("tbody tr:nth-child(4) td:nth-child(5)").should("have.text", "bemail1@example.com")
+    })
+
+    it("should not allow account with same username to be created", () => {
+      cy.visit("users/newUser")
+
+      cy.get('input[id="username"]').type("Bichard01")
+      cy.get('input[id="forenames"]').type("B forename")
+      cy.get('input[id="surname"]').type("B surname")
+      cy.get('input[id="phoneNumber"]').type("B phone")
+      cy.get('input[id="emailAddress"]').type("bemail2@example.com")
+
+      cy.get('input[id="postalAddress"]').type("B Address")
+      cy.get('input[id="postCode"]').type("B Code")
+      cy.get('input[id="endorsedBy"]').type("B Endorsed")
+      cy.get('input[id="orgServes"]').type("B organisation")
+
+      cy.get("button").click()
+      cy.get('span[id="event-name-error"]').should("have.text", "Error: Username Bichard01 already exists")
+    })
   })
 })
