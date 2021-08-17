ffaf441618a9454583d9f917d86ffd8937a38335
Emad Karamad <emad.karamad@madetech.com>
2021-08-17 10:15:50 +0100

    Tidy up new cypress tests

--- cypress/integration/forgot-password.spec.js
+++ cypress/integration/forgot-password.spec.js
@@ -1,30 +1,28 @@
-describe("User", () => {
+describe("Forgot password", () => {
   context("720p resolution", () => {
+    before(() => {
+      cy.task("seedUsers")
+    })
+
     beforeEach(() => {
       cy.viewport(1280, 720)
     })
 
-    describe("Forgot password page", () => {
-      before(() => {
-        cy.task("seedUsers")
-      })
+    it("should prompt the user to check their email after entering an email address", () => {
+      cy.visit("/login")
+      cy.get("a[href='/login/forgot-password']").click()
+      cy.get("body").contains(/forgot password/i)
+      cy.get("input[type=email]").type("bichard01@example.com")
+      cy.get("button[type=submit]").click()
+      cy.get("body").contains(/check your email/i)
+    })
 
-      it("should prompt the user to check their email after entering an email address", () => {
-        cy.visit("/login")
-        cy.get("a[href='/login/forgot-password']").click()
-        cy.get("body").contains(/forgot password/i)
-        cy.get("input[type=email]").type("bichard01@example.com")
-        cy.get("button[type=submit]").click()
-        cy.get("body").contains(/check your email/i)
-      })
-
-      it("should not allow submission of something that isn't an email address", () => {
-        cy.visit("/login")
-        cy.get("a[href='/login/forgot-password']").click()
-        cy.get("input[type=email]").type("foobar")
-        cy.get("button[type=submit]").click()
-        cy.url().should("match", /\/forgot-password\/?$/)
-      })
+    it("should not allow submission of something that isn't an email address", () => {
+      cy.visit("/login")
+      cy.get("a[href='/login/forgot-password']").click()
+      cy.get("input[type=email]").type("foobar")
+      cy.get("button[type=submit]").click()
+      cy.url().should("match", /\/forgot-password\/?$/)
     })
   })
 })
