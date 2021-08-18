938e151f42ae44c76d9511c5ba0ceb66b422df6e
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-08-18 16:48:20 +0100

    Delete the previous create Singleton

--- src/lib/createSingleton.ts
+++
@@ -1,9 +0,0 @@
-export default function createSingleton<T>(name: string, create: () => T): T {
-  const s = Symbol.for(name)
-  let scope = (global as any)[s]
-  if (!scope) {
-    scope = { ...create() }
-    ;(global as any)[s] = scope
-  }
-  return scope
-}
