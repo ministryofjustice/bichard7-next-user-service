6e62f16b88baf8ca7c181052432e721cb972436a
Emad Karamad <emad.karamad@madetech.com>
2021-08-19 13:23:11 +0100

    Update tokens in cypress checkCsrf task to match the new format

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,13 +30,13 @@ Cypress.Commands.add("checkCsrf", (url, method) => {
     method,
     url,
     headers: {
-      cookie: "XSRF-TOKEN%2Flogin=7tyFoLsw-L1-NzWTjPCnTf7YjDNGMAbd8KmU.cmu8gTFgJjXa8insESx4fsNn9jBJL9R3uD%2Be0yb26Es"
+      cookie: "XSRF-TOKEN%2Flogin=JMHZOOog-n0ZMO-UfRCZTCUxiQutsEeLpS8I.CJOHfajQ2zDKOZPaBh5J8VT%2FK4UrG6rB6o33VIvK04g"
     },
     form: true,
     followRedirect: false,
     body: {
       "XSRF-TOKEN":
-        "XSRF-TOKEN%2Flogin=TsRDivzH-KLjL_PkicOeXYK0velCTKJz7tBo.sN/lvve0ApcIfIShux0He7AVY1KIbLPlIPhEkATJqiU"
+        "XSRF-TOKEN%2Flogin=1629375460103.JMHZOOog-n0ZMO-UfRCZTCUxiQutsEeLpS8I.7+42/hdHVuddtxLw8IvGvIPVhkFj6kbvYukS1mGm64o"
     }
   }).then((withTokensResponse) => {
     expect(withTokensResponse.status).to.eq(403)
