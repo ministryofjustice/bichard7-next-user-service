cfec460abf5350014c9f0ee950d16cae316712e3
Emad Karamad <emad.karamad@madetech.com>
2021-08-20 11:33:12 +0100

    Feature/cypress tests for delete user pages (#58)
    * Add Cypress tests for delete user pages
    
    * Run unit and integration tests serially

--- cypress/integration/delete-user.spec.js
+++ cypress/integration/delete-user.spec.js
@@ -1,6 +1,38 @@
 describe("Delete user", () => {
-  before(async () => {
+  beforeEach(async () => {
     await cy.task("seedUsers")
+  })
+
+  it("should delete the user when confirmation text is valid", () => {
+    cy.visit("/users/Bichard02")
+    cy.get('a[data-test="delete-user-view"]').click()
+    cy.get("h1").contains("Are you sure you want to delete Bichard User 02 Surname 02?")
+    cy.get("input[id=delete-account-confirmation]").type("Bichard02")
+    cy.get("button[type=submit]").click()
+    cy.get(".govuk-notification-banner").contains("User 'Bichard User 02 Surname 02' has been deleted successfully.")
+    cy.get("a.govuk-back-link").click()
+    cy.url().should("contains", "/users")
+  })
+
+  it("should not allow deleting the user when confirmation text is invalid", () => {
+    cy.visit("/users/Bichard02")
+    cy.get('a[data-test="delete-user-view"]').click()
+    cy.get("h1").contains("Are you sure you want to delete Bichard User 02 Surname 02?")
+    cy.get("input[id=delete-account-confirmation]").type("Invalid input")
+    cy.get("button[type=submit]").click()
+    cy.get(".govuk-error-summary").contains("Username mismatch")
+    cy.get(".govuk-error-summary").contains("The provided username in the confirmation box does not match.")
+    cy.url().should("contains", "/users/Bichard02/delete")
+  })
+
+  it("should not allow deleting the user when confirmation text is empty", () => {
+    cy.visit("/users/Bichard02")
+    cy.get('a[data-test="delete-user-view"]').click()
+    cy.get("h1").contains("Are you sure you want to delete Bichard User 02 Surname 02?")
+    cy.get("button[type=submit]").click()
+    cy.get(".govuk-error-summary").contains("Username mismatch")
+    cy.get(".govuk-error-summary").contains("The provided username in the confirmation box does not match.")
+    cy.url().should("contains", "/users/Bichard02/delete")
   })
 
   it("should respond with forbidden response code when CSRF tokens are invalid in delete page", (done) => {
