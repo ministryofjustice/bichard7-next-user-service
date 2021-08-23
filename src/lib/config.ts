082adc8da86850616b0f159396371e9af61023bb
Emad Karamad <emad.karamad@madetech.com>
2021-08-23 13:36:48 +0100

    Sort config fields

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,33 +10,33 @@ interface SmtpConfig {
 }
 
 export interface UserServiceConfig {
+  authenticationCookieName: string
   bichardRedirectURL: string
-  redirectAccessList: string
   database: DatabaseConfig
   emailFrom: string
   emailVerificationExpiresIn: number
   incorrectDelay: number
+  redirectAccessList: string
   smtp: SmtpConfig
   tokenExpiresIn: string
   tokenIssuer: string
   tokenQueryParamName: string
   tokenSecret: string
-  authenticationCookieName: string
   verificationCodeLength: number
   csrf: CsrfConfig
 }
 
 const config: UserServiceConfig = {
+  authenticationCookieName: process.env.AUTH_COOKIE_NAME ?? ".AUTH",
   bichardRedirectURL: process.env.BICHARD_REDIRECT_URL ?? "https://localhost:9443/bichard-ui/Authenticate",
-  redirectAccessList: process.env.BICHARD_REDIRECT_ACCESS_LIST ?? "localhost,",
   emailFrom: `Bichard <${process.env.EMAIL_FROM ?? "bichard@cjse.org"}>`,
   emailVerificationExpiresIn: parseInt(process.env.EMAIL_VERIFICATION_EXPIRY ?? "30", 10),
   incorrectDelay: parseInt(process.env.INCORRECT_DELAY ?? "10", 10),
+  redirectAccessList: process.env.BICHARD_REDIRECT_ACCESS_LIST ?? "localhost,",
   tokenExpiresIn: process.env.TOKEN_EXPIRES_IN ?? "15 seconds",
   tokenIssuer: process.env.TOKEN_ISSUER ?? "Bichard",
   tokenQueryParamName: process.env.TOKEN_QUERY_PARAM_NAME ?? "token",
   tokenSecret: process.env.TOKEN_SECRET ?? "OliverTwist",
-  authenticationCookieName: process.env.AUTH_COOKIE_NAME ?? ".AUTH",
   verificationCodeLength: 6,
   csrf: {
     tokenName: process.env.CSRF_COOKIE_NAME ?? "XSRF-TOKEN",
