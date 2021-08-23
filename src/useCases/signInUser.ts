7330de3de78d9a57de80577cded412e71c4b6a0d
Emad Karamad <emad.karamad@madetech.com>
2021-08-23 12:45:35 +0100

    Update signInUser to use authenticationCookieName

--- src/useCases/signInUser.ts
+++ src/useCases/signInUser.ts
@@ -1,11 +1,16 @@
 import { serialize } from "cookie"
 import { ServerResponse } from "http"
+import config from "lib/config"
 import { generateAuthenticationToken } from "lib/token/authenticationToken"
 import User from "types/User"
 
 export default (response: ServerResponse, user: User): string => {
+  const { authenticationCookieName } = config
   const authenticationToken = generateAuthenticationToken(user)
-  response.setHeader("Set-Cookie", serialize(".AUTH", authenticationToken, { httpOnly: true }))
+  response.setHeader(
+    "Set-Cookie",
+    serialize(authenticationCookieName, authenticationToken, { httpOnly: true, path: "/" })
+  )
 
   return authenticationToken
 }
