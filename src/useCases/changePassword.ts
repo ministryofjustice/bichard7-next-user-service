9aa16dfb5c643900cd24309ba6c68e0ce552baab
Emad Karamad <emad.karamad@madetech.com>
2021-08-24 11:49:10 +0100

    Add password security check to changePassword use case

--- src/useCases/changePassword.ts
+++ src/useCases/changePassword.ts
@@ -1,6 +1,7 @@
 import Database from "types/Database"
 import { isError, PromiseResult } from "types/Result"
 import checkPassword from "./checkPassword"
+import passwordSecurityCheck from "./passwordSecurityCheck"
 import sendPasswordChangedEmail from "./sendPasswordChangedEmail"
 import updateUserPassword from "./updateUserPassword"
 
@@ -10,6 +11,12 @@ export default async (
   currentPassword: string,
   newPassword: string
 ): PromiseResult<void> => {
+  const passwordCheckResult = passwordSecurityCheck(newPassword)
+
+  if (isError(passwordCheckResult)) {
+    return passwordCheckResult
+  }
+
   const passwordMatch = await checkPassword(connection, emailAddress, currentPassword)
 
   if (isError(passwordMatch)) {
