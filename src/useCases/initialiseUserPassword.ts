3f65af9d843d0277618f006fd1f6307d6f3bbc84
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-09-01 13:55:00 +0100

    Adding logic to not allow unsafe passwords

--- src/useCases/initialiseUserPassword.ts
+++ src/useCases/initialiseUserPassword.ts
@@ -1,5 +1,6 @@
 import Database from "types/Database"
 import { isError, PromiseResult } from "types/Result"
+import passwordDoesNotContainSensitive from "./passwordDoesNotContainSensitive"
 import passwordSecurityCheck from "./passwordSecurityCheck"
 import storePasswordResetCode from "./storePasswordResetCode"
 import updatePassword from "./updatePassword"
@@ -16,19 +17,21 @@ const initialiseUserPassword = async (
     return passwordCheckResult
   }
 
-  // check if we have the correct user
   const validatedCodeResult = await validateUserVerificationCode(connection, emailAddress, verificationCode)
   if (isError(validatedCodeResult)) {
     return new Error("Invalid or expired verification code")
   }
 
-  // set verification code to empty string
+  const validatePasswordSensitveResult = await passwordDoesNotContainSensitive(connection, password, emailAddress)
+  if (isError(validatePasswordSensitveResult)) {
+    return validatePasswordSensitveResult
+  }
+
   const resetResult = await storePasswordResetCode(connection, emailAddress, null)
   if (isError(resetResult)) {
     return new Error("Failed to update table")
   }
 
-  // set the new password
   const updateResult = await updatePassword(connection, emailAddress, password)
   if (isError(updateResult)) {
     return new Error("Failed to update password")
