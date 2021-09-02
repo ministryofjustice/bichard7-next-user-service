3721fac8e0a367c3df9b58ecb30d2c291b92fd43
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-09-02 10:07:57 +0100

    Finalising usecase for banned passwords

---
+++ src/useCases/checkPasswordIsBanned.ts
@@ -0,0 +1,14 @@
+import bannedPasswords from "lib/bannedPasswords"
+import { Result } from "types/Result"
+
+const bannedPasswordsDictionary = Object.assign({}, ...bannedPasswords.split("\n").map((word) => ({ [word]: true })))
+
+const checkPasswordIsBanned = (newPassword: string): Result<void> => {
+  if (newPassword in bannedPasswordsDictionary) {
+    return Error("Cannot use this password as it is unsecure")
+  }
+
+  return undefined
+}
+
+export default checkPasswordIsBanned
