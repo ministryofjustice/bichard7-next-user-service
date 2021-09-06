a39bea758d9038079e0addbe4a8538c23609fe73
Emad Karamad <emad.karamad@madetech.com>
2021-09-06 20:33:33 +0100

    Tidy up getAuditLogger.ts

--- src/lib/getAuditLogger.ts
+++ src/lib/getAuditLogger.ts
@@ -1,22 +1,29 @@
 import { GetServerSidePropsContext } from "next"
 import AuditLogger from "types/AuditLogger"
+import { PromiseResult } from "types/Result"
 import { UserServiceConfig } from "./config"
 import generateAuditLog from "./generateAuditLog"
 
 const getConsoleAuditLogger =
   (context: GetServerSidePropsContext): AuditLogger =>
-  // eslint-disable-next-line require-await
-  async (action, attributes) => {
-    const auditLog = generateAuditLog(context, action, attributes)
+  (action, attributes): PromiseResult<void> => {
+    try {
+      const auditLog = generateAuditLog(context, action, attributes)
 
-    console.log("[Audit Logger]")
-    console.log(`Audit Log ID:  ${auditLog.auditLogId}`)
-    console.log(`Timestamp:     ${auditLog.timestamp.toISOString()}`)
-    console.log(`Action:        ${auditLog.action}`)
-    console.log(`Username:      ${auditLog.username}`)
-    console.log(`User IP:       ${auditLog.userIp}`)
-    console.log(`Request URI:   ${auditLog.requestUri}`)
-    console.log(`\n${JSON.stringify(auditLog.attributes)}`)
+      console.log("[Audit Logger]")
+      console.log(`Audit Log ID:  ${auditLog.auditLogId}`)
+      console.log(`Timestamp:     ${auditLog.timestamp.toISOString()}`)
+      console.log(`Action:        ${auditLog.action}`)
+      console.log(`Username:      ${auditLog.username}`)
+      console.log(`User IP:       ${auditLog.userIp}`)
+      console.log(`Request URI:   ${auditLog.requestUri}`)
+      console.log(`\n${JSON.stringify(auditLog.attributes)}`)
+    } catch (error) {
+      console.error(error)
+      return Promise.resolve(error)
+    }
+
+    return Promise.resolve()
   }
 
 export default (context: GetServerSidePropsContext, config: UserServiceConfig): AuditLogger => {
