35058d4ed8bbd503ea59e8bf61b6c90baf85ad40
Emad Karamad <emad.karamad@madetech.com>
2021-10-12 10:37:37 +0100

    Limit access to audit logging (#119)
    * Limit access to audit logging
    
    * Update nginx gid to 999 in goss.yaml

--- src/useCases/hasUserAccessToUrl.ts
+++ src/useCases/hasUserAccessToUrl.ts
@@ -3,17 +3,19 @@ import getUserServiceAccess from "./getUserServiceAccess"
 
 const userManagementUrlExpression = /^\/users\/users.*/
 const bichardUrlExpression = /^\/bichard-ui.*/
+const auditLoggingUrlExpression = /^\/audit-logging.*/
 
 export default (token: AuthenticationTokenPayload, url?: string): boolean => {
   if (!url) {
     return false
   }
 
-  const { hasAccessToBichard, hasAccessToUserManagement } = getUserServiceAccess(token)
+  const { hasAccessToBichard, hasAccessToUserManagement, hasAccessToAuditLogging } = getUserServiceAccess(token)
 
   if (
     (url.match(userManagementUrlExpression) && !hasAccessToUserManagement) ||
-    (url.match(bichardUrlExpression) && !hasAccessToBichard)
+    (url.match(bichardUrlExpression) && !hasAccessToBichard) ||
+    (url.match(auditLoggingUrlExpression) && !hasAccessToAuditLogging)
   ) {
     return false
   }
