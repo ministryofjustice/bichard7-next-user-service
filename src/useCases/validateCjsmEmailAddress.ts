236dbd54f45c28e86640dc57a57c6906fe959fe2
Emad Karamad <emad.karamad@madetech.com>
2021-10-13 14:13:44 +0100

    Revert "Add validateCjsmEmailAddress use case"
    This reverts commit 6b0ab4821e3c720bb7b38b2ec30dc264960e405a.

--- src/useCases/validateCjsmEmailAddress.ts
+++
@@ -1,7 +0,0 @@
-import { UserServiceConfig } from "lib/config"
-
-export default (config: UserServiceConfig, emailAddress: string): boolean => {
-  const { validateCjsmEmailAddress } = config
-
-  return validateCjsmEmailAddress !== "true" || !!emailAddress.match(/^.*cjsm.net$/)
-}
