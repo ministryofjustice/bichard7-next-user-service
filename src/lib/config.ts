924b06e73ebe414bb74604bce0168ac2bd10e179
Emad Karamad <emad.karamad@madetech.com>
2021-10-13 14:13:51 +0100

    Revert "Add validate CJSM address to config"
    This reverts commit 7f4e99764dfc2212871cfe82623fb0972cdf122d.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -38,7 +38,6 @@ export interface UserServiceConfig {
   tokenSecret: string
   maxServiceMessagesPerPage: number
   maxUsersPerPage: number
-  validateCjsmEmailAddress: string
   verificationCodeLength: number
 }
 
@@ -73,7 +72,6 @@ const config: UserServiceConfig = {
   tokenSecret: process.env.TOKEN_SECRET ?? "OliverTwist",
   maxServiceMessagesPerPage: 5,
   maxUsersPerPage: 10,
-  validateCjsmEmailAddress: process.env.EMAIL_VALIDATE_CJSM_ADDRESS ?? "false",
   verificationCodeLength: 6,
   csrf: {
     tokenName: "CSRFToken",
