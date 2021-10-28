669def15ab84424228f6721658cbff7cea9903bf
Ben Pirt <ben@pirt.co.uk>
2021-10-28 09:01:09 +0100

    Remove the access-denied page

--- test/pages/access-denied.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react"
-import AccessDenied from "pages/access-denied"
-
-it("should render the component and match the snapshot", () => {
-  const { container } = render(<AccessDenied />)
-
-  expect(container).toMatchSnapshot()
-})
