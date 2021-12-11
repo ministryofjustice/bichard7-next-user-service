ba919d4bd52ca89098c88fc0629945aa2009173f
Ben Pirt <ben@pirt.co.uk>
2021-12-11 14:46:57 +0000

    Run the Cypress tests in parallel

--- scripts/run-test-ui.sh
+++ scripts/run-test-ui.sh
@@ -4,9 +4,15 @@ set -e
 npm run build
 
 START_CMD="npm:start > /dev/null 2>&1"
+TOTAL_CHUNKS=${TOTAL_CHUNKS:-1}
+CHUNK_NUMBER=${CHUNK_NUMBER:-0}
+
+echo "TOTAL_CHUNKS: ${TOTAL_CHUNKS}"
+echo "CHUNK_NUMBER: ${CHUNK_NUMBER}"
 
 if [ $# -eq 0 ]; then
-    npx concurrently --raw --kill-others --success first "$START_CMD" "npm:cypress:run"
+    CHUNKS=$(find cypress/integration/** -iname '*.spec.js' | sort | awk "(NR % $TOTAL_CHUNKS == $CHUNK_NUMBER)" | paste -d ' ' -s -)
+    npx concurrently --raw --kill-others --success first "$START_CMD" "npm:cypress:run:file ${CHUNKS}"
 else
     FILES=""
     for f in "$@"; do
