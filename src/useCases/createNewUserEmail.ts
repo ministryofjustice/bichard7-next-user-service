4ab328108658332a2f77187a2573e73042f8c60f
Sam Blackwell <samuel.blackwell@madetech.com>
2021-12-19 14:13:56 -0800

    change create new user email use case to send user to change-password view

--- src/useCases/createNewUserEmail.ts
+++ src/useCases/createNewUserEmail.ts
@@ -1,23 +1,11 @@
 import generateNewUserEmail from "emails/newUser"
-import { EmailVerificationTokenPayload, generateEmailVerificationToken } from "lib/token/emailVerificationToken"
 import { addBasePath } from "next/dist/shared/lib/router/router"
 import EmailContent from "types/EmailContent"
-import { isError, Result } from "types/Result"
+import { Result } from "types/Result"
 import UserCreateDetails from "types/UserDetails"
 
-export default (user: UserCreateDetails, verificationCode: string, baseUrl: string): Result<EmailContent> => {
-  const payload: EmailVerificationTokenPayload = {
-    emailAddress: user.emailAddress,
-    verificationCode
-  }
-
-  const token = generateEmailVerificationToken(payload)
-  if (isError(token)) {
-    return token
-  }
-
-  const url = new URL(addBasePath("/login/new-password"), baseUrl)
-  url.searchParams.append("token", token)
+export default (user: UserCreateDetails, baseUrl: string): Result<EmailContent> => {
+  const url = new URL(addBasePath("/account/change-password"), baseUrl)
 
   return generateNewUserEmail({ url: url.href, user })
 }
