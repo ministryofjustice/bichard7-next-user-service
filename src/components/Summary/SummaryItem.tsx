6604983ebddd2263f7b96f5478d089915c841cbe
Sam Blackwell <samuel.blackwell@madetech.com>
2021-12-19 15:18:05 -0800

    add data-test capability to SummaryItem component

--- src/components/Summary/SummaryItem.tsx
+++ src/components/Summary/SummaryItem.tsx
@@ -1,12 +1,17 @@
 interface Props {
   label: string
   value: string
+  dataTest: string
 }
 
-const SummaryItem = ({ label, value }: Props) => (
+const SummaryItem = ({ label, value, dataTest }: Props) => (
   <div className="govuk-summary-list__row">
-    <dt className="govuk-summary-list__key">{label}</dt>
-    <dd className="govuk-summary-list__value">{value}</dd>
+    <dt data-test={`summary-item_${dataTest}_label`} className="govuk-summary-list__key">
+      {label}
+    </dt>
+    <dd data-test={`summary-item_${dataTest}_value`} className="govuk-summary-list__value">
+      {value}
+    </dd>
   </div>
 )
 
