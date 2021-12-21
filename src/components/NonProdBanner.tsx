5208610da9057c75cd7bee24cff8c3602194261d
Jamie Davies <jamie.davies@madetech.com>
2021-12-21 16:25:07 +0000

    Add component-level styles instead of inline styles to avoid CSP issues

--- src/components/NonProdBanner.tsx
+++ src/components/NonProdBanner.tsx
@@ -1,18 +1,14 @@
 import config from "lib/config"
-
-const whiteTextStyle = { color: "#fff" }
-const redBackgroundStyle = { backgroundColor: "#d4351c" }
+import styles from "../styles/components/NonProdBanner.module.css"
 
 const NonProdBanner = () => (
-  <div className="govuk-phase-banner" style={redBackgroundStyle}>
+  <div className={`govuk-phase-banner ${styles.nonProdBanner}`}>
     <div className="govuk-width-container ">
       <p className="govuk-phase-banner__content">
         <strong className="govuk-tag govuk-tag--red govuk-phase-banner__content__tag">{"development"}</strong>
-        <span className="govuk-phase-banner__text" style={whiteTextStyle}>
+        <span className="govuk-phase-banner__text">
           {"This is a development version of Bichard that is being used for testing. "}
-          <a href={config.productionUrl} style={whiteTextStyle}>
-            {"Go to production instead."}
-          </a>
+          <a href={config.productionUrl}>{"Go to production instead."}</a>
         </span>
       </p>
     </div>
