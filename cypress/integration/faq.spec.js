d0247892a796dfe7e97c599ee6f023ad0a2f9cb5
Sam Blackwell <samuel.blackwell@madetech.com>
2022-01-04 22:40:30 +0000

    lint fixes

--- cypress/integration/faq.spec.js
+++ cypress/integration/faq.spec.js
@@ -1,7 +1,23 @@
-describe("403 - Access denied", () => {
-  it("should display the correct 403 page", () => {
+describe("FAQ", () => {
+  it("should display the FAQ title", () => {
+    // When
     cy.visit("/faq")
+    // Then
+    cy.get("[data-test='faq_heading']").should("have.text", "FAQ")
+  })
 
-    cy.get("head title").should("have.text", "Access denied")
+  it("should display the correct last updated information", () => {
+    // When
+    cy.visit("/faq")
+    // Then
+    cy.get("[data-test='faq_last-updated']").should("contain", "04-01-2022")
+  })
+
+  it("should display the expected 5 questions and answers", () => {
+    // When
+    cy.visit("/faq")
+    // Then
+    cy.get("[data-test='faq_question']").should("have.length", "5")
+    cy.get("[data-test='faq_answer']").should("have.length", "5")
   })
 })
