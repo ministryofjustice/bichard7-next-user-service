f49fd69585db77069e4b314df89b77bd881bb22a
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2022-01-12 11:06:44 +0000

    Simplify function code

--- src/components/UserManagers.tsx
+++ src/components/UserManagers.tsx
@@ -2,12 +2,10 @@ type Props = {
   userManagerNames: string
 }
 
-const UserManagers = ({ userManagerNames }: Props) => {
-  return (
-    <p className="govuk-body">
-      {`If you have any queries about your permissions or cannot see the resources you expect, please contact one of the user managers for your force: ${userManagerNames}.`}
-    </p>
-  )
-}
+const UserManagers = ({ userManagerNames }: Props) => (
+  <p className="govuk-body">
+    {`If you have any queries about your permissions or cannot see the resources you expect, please contact one of the user managers for your force: ${userManagerNames}.`}
+  </p>
+)
 
 export default UserManagers
