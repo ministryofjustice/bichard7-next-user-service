9e4a4d272d3292a71d4adc9873055ee1a8814fd7
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2022-01-14 15:47:32 +0000

    Adding SQL with recursion to code

--- src/useCases/getUserGroups.ts
+++ src/useCases/getUserGroups.ts
@@ -4,16 +4,33 @@ import PromiseResult from "types/PromiseResult"
 
 const getUserGroups = (connection: Database, usernames: string[]): PromiseResult<UserGroupResult[]> => {
   const getUserGroupsQuery = `
+    WITH RECURSIVE name_tree AS ( 
       SELECT DISTINCT
         g.id,
         g.name,
-        g.friendly_name
+        g.friendly_name,
+        g.parent_id
       FROM br7own.groups AS g
-      INNER JOIN br7own.users_groups AS ug ON ug.group_id = g.id
-      INNER JOIN br7own.users AS u ON u.id = ug.user_id
+        INNER JOIN br7own.users_groups AS ug ON ug.group_id = g.id
+        INNER JOIN br7own.users AS u ON u.id = ug.user_id
       WHERE u.username IN ($\{usernames:csv\})
-      ORDER BY g.name
-    `
+      UNION ALL
+        SELECT DISTINCT
+          g2.id,
+          g2.name,
+          g2.friendly_name,
+          g2.parent_id
+        FROM br7own.groups AS g2
+          JOIN name_tree p ON p.id = g2.parent_id
+    )
+    SELECT DISTINCT
+      id,
+      name,
+      friendly_name,
+      parent_id
+    FROM name_tree
+    OPTION (MAXRECURSION 8)
+  `
 
   return connection.any(getUserGroupsQuery, { usernames }).catch((error) => error as Error)
 }
--- testFixtures/database/insertIntoGroupsTable.js
+++ testFixtures/database/insertIntoGroupsTable.js
@@ -14,7 +14,103 @@ const insertIntoGroupTable = async (data) => {
       )
   `
 
-  const queries = data.map((datum) => connection.none(insertQuery, { ...datum }))
+  data.map((datum) => connection.none(insertQuery, { ...datum }))
+  const queries = []
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7SuperUserManager_grp'
+  )
+  WHERE name = 'B7AuditLoggingManager_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7SuperUserManager_grp'
+  )
+  WHERE name = 'B7UserManager_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7UserManager_grp'
+  )
+  WHERE name = 'B7Audit_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7UserManager_grp'
+  )
+  WHERE name = 'B7Supervisor_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7Supervisor_grp'
+  )
+  WHERE name = 'B7Allocator_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7Supervisor_grp'
+  )
+  WHERE name = 'B7GeneralHandler_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7GeneralHandler_grp'
+  )
+  WHERE name = 'B7TriggerHandler_grp'`),
+    {}
+  )
+
+  queries.push(
+    connection.none(`
+  UPDATE br7own.groups
+  SET parent_id = (
+      SELECT id
+      FROM br7own.groups
+      WHERE name = 'B7GeneralHandler_grp'
+  )
+  WHERE name = 'B7ExceptionHandler_grp'`),
+    {}
+  )
 
   return Promise.all(queries)
 }
