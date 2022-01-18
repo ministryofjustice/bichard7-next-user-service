ca62c1809baf6aed7930fbc1424a01c22117c9bf
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2022-01-18 16:52:01 +0000

    Putting repeating code into function for better re-usability

--- testFixtures/database/data/serviceMessages.js
+++ testFixtures/database/data/serviceMessages.js
@@ -1,99 +1,57 @@
+function getDate(daysInPast) {
+  const x = new Date()
+  x.setDate(x.getDate() - daysInPast)
+  return x
+}
+
 const serviceMessages = [
   {
     message: "Message 1",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 270)
-      return x
-    }
+    created_at: getDate(270)
   },
   {
     message: "Message 2",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 180)
-      return x
-    }
+    created_at: getDate(180)
   },
   {
     message: "Message 3",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 90)
-      return x
-    }
+    created_at: getDate(90)
   },
   {
     message: "Message 4",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 60)
-      return x
-    }
+    created_at: getDate(60)
   },
   {
     message: "Message 5",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 35)
-      return x
-    }
+    created_at: getDate(35)
   },
   {
     message: "Message 6",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 31)
-      return x
-    }
+    created_at: getDate(31)
   },
   {
     message: "Message 7",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 27)
-      return x
-    }
+    created_at: getDate(27)
   },
   {
     message: "Message 8",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 25)
-      return x
-    }
+    created_at: getDate(25)
   },
   {
     message: "Message 9",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 20)
-      return x
-    }
+    created_at: getDate(20)
   },
   {
     message: "Message 10",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 15)
-      return x
-    }
+    created_at: getDate(15)
   },
   {
     message: "Message 11",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 10)
-      return x
-    }
+    created_at: getDate(10)
   },
   {
     message: "Message 12",
-    created_at: () => {
-      const x = new Date()
-      x.setDate(x.getDate() - 5)
-      return x
-    }
+    created_at: getDate(5)
   },
   {
     message: "Message 13",
