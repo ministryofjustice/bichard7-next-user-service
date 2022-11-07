bd2ee15f74ee51ad9566912049e7151989831402
Alice Lee <alice.lee@madetech.com>
2022-11-07 11:03:07 +0000

    Fix eslint errors in cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "cypress"
+import setupCustomNodeEvents from "./cypress/plugins"
 
 export default defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config)
+      return setupCustomNodeEvents(on, config)
     },
     baseUrl: "http://localhost:3000/users"
   }
