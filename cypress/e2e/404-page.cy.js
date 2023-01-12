098fdea626d28dd8b61169b160f4ca853f9e7105
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-01-12 10:52:42 +0000

    Simplify 404 cy test

--- cypress/e2e/404-page.cy.js
+++ cypress/e2e/404-page.cy.js
@@ -2,20 +2,13 @@ describe("404 - Page not found", () => {
   it("should display the correct 404 page when an incorrect url has been requested", () => {
     cy.visit("/bad-url-request", { failOnStatusCode: false })
 
-    cy.get("head title").should("have.text", "Page not found")
-    cy.get("h1.govuk-heading-xl").should("have.text", "Page not found")
-    cy.get("p.govuk-body:nth-child(2)").should("have.text", "If you typed the web address, check it is correct.")
-    cy.get("p.govuk-body:nth-child(3)").should(
-      "have.text",
-      "If you pasted the web address, check you copied the entire address."
-    )
-    cy.get("p.govuk-body:nth-child(4)").should(
-      "have.text",
-      "If the web address is correct or you selected a link or button, contact support."
-    )
+    cy.contains("Page not found")
+    cy.contains("If you typed the web address, check it is correct.")
+    cy.contains("If you pasted the web address, check you copied the entire address.")
+    cy.contains("If the web address is correct or you selected a link or button, contact support.")
   })
 
-  it("should respond with correct HTTP status code (404)", () => {
+  it("should respond with the correct HTTP status code (404)", () => {
     cy.request({
       failOnStatusCode: false,
       url: "/bad-url-request"
