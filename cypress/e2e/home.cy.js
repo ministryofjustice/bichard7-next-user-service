20d0f21503993291932b3ce4131d514277af6adf
Donny <donny.hyon@madetech.com>
2023-01-12 11:08:32 +0000

    aligned specs to new format

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,48 +1,44 @@
 describe("Home", () => {
-  context("720p resolution", () => {
-    beforeEach(() => {
-      cy.resetTableToDefault()
-      cy.task("insertIntoUsersTable")
+  before(() => {
+    cy.resetTableToDefault()
+    cy.task("insertIntoUsersTable")
+    cy.task("insertIntoUserGroupsTable", {
+      email: "bichard01@example.com",
+      groups: ["B7UserManager_grp", "B7AuditLoggingManager_grp", "B7Supervisor_grp"]
+    })
+  })
 
-      cy.task("insertIntoUserGroupsTable", {
-        email: "bichard01@example.com",
-        groups: ["B7UserManager_grp", "B7AuditLoggingManager_grp", "B7Supervisor_grp"]
-      })
-      cy.viewport(1280, 720)
+  it("should redirect user to home page after successful login", () => {
+    const emailAddress = "bichard01@example.com"
+    cy.login(emailAddress, "password")
+    cy.get("h1").contains(/welcome bichard user 01/i)
+    cy.get("a[id=user-management-link]").should("have.attr", "href").and("equal", "/users/users")
+    cy.get("a[id=bichard-link]").should("have.attr", "href").and("equal", "/bichard-ui/InitialRefreshList")
+  })
+
+  it("should show paginated service messages", () => {
+    cy.login("bichard01@example.com", "password")
+    cy.get("body").contains("Latest service messages")
+    cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
+      cy.wrap(row)
+        .get(".govuk-body")
+        .contains(`Message ${13 - index}`)
     })
 
-    it("should redirect user to home page after successful login", () => {
-      const emailAddress = "bichard01@example.com"
-      cy.login(emailAddress, "password")
-      cy.get("h1").contains(/welcome bichard user 01/i)
-      cy.get("a[id=user-management-link]").should("have.attr", "href").and("equal", "/users/users")
-      cy.get("a[id=bichard-link]").should("have.attr", "href").and("equal", "/bichard-ui/InitialRefreshList")
+    cy.get('a[data-test="Next"]').click()
+
+    cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
+      cy.wrap(row)
+        .get(".govuk-body")
+        .contains(`Message ${8 - index}`)
     })
 
-    it("should show paginated service messages", () => {
-      cy.login("bichard01@example.com", "password")
-      cy.get("body").contains("Latest service messages")
-      cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
-        cy.wrap(row)
-          .get(".govuk-body")
-          .contains(`Message ${13 - index}`)
-      })
+    cy.get('a[data-test="Prev"]').click()
 
-      cy.get('a[data-test="Next"]').click()
-
-      cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
-        cy.wrap(row)
-          .get(".govuk-body")
-          .contains(`Message ${8 - index}`)
-      })
-
-      cy.get('a[data-test="Prev"]').click()
-
-      cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
-        cy.wrap(row)
-          .get(".govuk-body")
-          .contains(`Message ${13 - index}`)
-      })
+    cy.get(".govuk-grid-column-one-third > .govuk-grid-row").each((row, index) => {
+      cy.wrap(row)
+        .get(".govuk-body")
+        .contains(`Message ${13 - index}`)
     })
   })
 })
