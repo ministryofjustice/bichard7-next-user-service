ad68222a77c0fb7faf5e180a8e71686722aed99e
Donny <donny.hyon@madetech.com>
2023-01-12 16:19:13 +0000

    refactored method of getting href

--- cypress/e2e/viewUser.cy.js
+++ cypress/e2e/viewUser.cy.js
@@ -19,7 +19,7 @@ describe("Viewing a single user", () => {
   it("should prevent user from clicking on delete user button", () => {
     cy.login("bichard01@example.com", "password")
     cy.get("#user-management-link").click()
-    cy.get('a[href="users/Bichard01"]').click()
+    cy.get('a[href*="Bichard01"]').click()
     cy.get('[data-test="disabled-delete-anchor"]').should("be.visible")
   })
 
@@ -27,7 +27,7 @@ describe("Viewing a single user", () => {
     cy.login("bichard01@example.com", "password")
 
     cy.get("#user-management-link").click()
-    cy.get('a[href="users/Bichard01"]').click()
+    cy.get('a[hre*f="Bichard01"]').click()
 
     cy.get('[data-test="summary-item_username_value"]').should("be.visible").contains("Bichard01")
     cy.get('[data-test="summary-item_forename_value"]').should("be.visible").contains("Bichard User 01")
