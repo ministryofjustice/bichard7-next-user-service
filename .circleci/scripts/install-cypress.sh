99adddcea12d83ff77b8afe9f67d165171f40a2f
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-11-03 17:19:39 +0000

    Prevent interactive service restart popup when running apt-update

--- .circleci/scripts/install-cypress.sh
+++ .circleci/scripts/install-cypress.sh
@@ -3,7 +3,7 @@
 echo "Installing Cypress..."
 
 # Install Cypress dependencies
-sudo apt-get update
+sudo NEEDRESTART_MODE=a apt-get update
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 
 # Install Cypress
