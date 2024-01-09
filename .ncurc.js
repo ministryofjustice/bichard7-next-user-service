a6ddfce0519a580eafe8ec48b8deeda78b3201e1
Richard Cane <richard.cane@madetech.com>
2024-01-09 15:44:26 +0000

    Clean up ncurc

--- .ncurc.js
+++ .ncurc.js
@@ -1,26 +1,23 @@
-/*
-  Skipped:
-  - next
-    - 13.4.13 causes failures with fetch
-*/
 const pinned = []
 const ignored = []
-const skipped = [{ package: "next", version: "13.4.13" }]
+const skipped = [
+  /*{ package: "next", version: "13.4.13" }*/
+]
 
 module.exports = {
-  target: (package) => {
-    if (pinned.some((pin) => pin === package)) {
+  target: (pkg) => {
+    if (pinned.some((pin) => pin === pkg)) {
       const res = "minor"
-      console.log(` ${package} is pinned to ${res} upgrades only (.ncurc.js)`)
+      console.log(` ${pkg} is pinned to ${res} upgrades only (.ncurc.js)`)
       return res
     }
     return "latest"
   },
 
-  filterResults: (package, { upgradedVersion }) => {
-    if (ignored.some((ignore) => ignore.package === package)) {
+  filterResults: (pkg, { upgradedVersion }) => {
+    if (ignored.some((ignore) => ignore.package === pkg)) {
       return
-    } else if (skipped.some((skip) => skip.package === package && skip.version === upgradedVersion)) {
+    } else if (skipped.some((skip) => skip.package === pkg && skip.version === upgradedVersion)) {
       return
     }
     return true
