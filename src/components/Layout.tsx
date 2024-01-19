bc298b480cafa80c27223b8fdfd0cf189a00ea5b
Richard Race <1273965+richrace@users.noreply.github.com>
2024-01-19 16:38:39 +0000

    Add connecting react bits

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,16 @@
 import Footer from "components/Footer"
 import Header from "components/Header"
+import { addBasePath } from "next/dist/client/add-base-path"
 import { ReactNode } from "react"
 import User from "types/User"
-import { addBasePath } from "next/dist/client/add-base-path"
+import NavBar from "./NavBar"
 
 interface Props {
   children: ReactNode
   user?: Partial<User>
+  hasAccessToReports?: boolean
+  hasAccessToUserManagement?: boolean
+  hasAccessToNewBichard?: boolean
 }
 
 /* eslint-disable jsx-a11y/alt-text, @next/next/no-img-element */
@@ -19,10 +23,16 @@ const FakeAssetForNoJsStatsGathering = () => (
 const ScreenSizeStats = () => <script src={addBasePath("/js/grabScreenSize.js")} async />
 
 /* eslint-enable jsx-a11y/alt-text, @next/next/no-img-element */
-const Layout = ({ children, user }: Props) => (
+const Layout = ({ children, user, hasAccessToReports, hasAccessToUserManagement, hasAccessToNewBichard }: Props) => (
   <>
     <FakeAssetForNoJsStatsGathering />
-    <Header serviceName="Ministry of Justice" user={user} />
+    <Header serviceName="Bichard7" userName={user?.username ?? ""} organisationName={"Ministry of Justice"} />
+    {hasAccessToNewBichard ? (
+      <NavBar
+        hasAccessToReports={hasAccessToReports ?? false}
+        hasAccessToUserManagement={hasAccessToUserManagement ?? false}
+      />
+    ) : undefined}
 
     <div className="govuk-width-container">
       <main className="govuk-main-wrapper" role="main">
