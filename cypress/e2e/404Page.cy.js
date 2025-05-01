e354cc48f8d9b5318e6ba54eb8abaefaf22f59bf
Richard Race <1273965+richrace@users.noreply.github.com>
2025-05-01 16:32:36 +0100

    Add cookie if we get 404 on court details page

--- cypress/e2e/404Page.cy.js
+++ cypress/e2e/404Page.cy.js
@@ -16,4 +16,24 @@ describe("404 - Page not found", () => {
       expect(response.status).to.eq(404)
     })
   })
+
+  it("should create a cookie on 404 if the path is /bichard/court-cases/<num>", () => {
+    cy.visit("/bichard/court-cases/733", { failOnStatusCode: false })
+
+    cy.contains("Page not found")
+    cy.get("#cookie-set")
+
+    cy.getCookie("qa_case_details_404")
+      .should("exist")
+      .then((c) => expect(c.value).to.include("/bichard/court-cases/733"))
+  })
+
+  it("should not create a cookie on 404 if the path is not /bichard/court-cases/<num>", () => {
+    cy.visit("/bichard/something", { failOnStatusCode: false })
+
+    cy.contains("Page not found")
+    cy.get("#cookie-set").should("not.exist")
+
+    cy.getCookie("qa_case_details_404").should("not.exist")
+  })
 })
